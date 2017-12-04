7b81091069f57bcf695ba264600d2feaf792d718
Bartek Igielski <igloczek@gmail.com>
2017-12-04 11:58:32 +0100

    Proper key value in for loop

--- src/components/YtComments.vue
+++ src/components/YtComments.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ytd-item-section-renderer style-scope ytd-comments">
-    <ul v-show="commentsCount && !loading" v-for="comment in comments" v-bind:key="comment">
+    <ul v-show="commentsCount && !loading" v-for="comment in comments" v-bind:key="comment.id">
       <li>{{ comment }}</li>
     </ul>
   </div>
