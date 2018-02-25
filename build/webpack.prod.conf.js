3c720a94de28d1856f5514644c45fbc94743e761
Bartek Igielski <igloczek@gmail.com>
2018-02-26 00:08:04 +0100

    HTML precashing disabled to simplify cache busting

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -99,7 +99,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'my-vue-app',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['dist/**/*.{js,css}'],
       minify: true,
       stripPrefix: 'dist/'
     })
