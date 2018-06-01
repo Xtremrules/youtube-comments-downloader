4a94088de6bb3356e4e347f7be4cbd596e5dfd29
Bartek Igielski <igloczek@gmail.com>
2018-06-01 16:53:25 +0200

    Preview component

---
+++ src/components/preview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import * as firebase from 'firebase'
+
+export default {
+  data() {
+    return {
+      videoId: 'lqIDULLoZn4'
+    }
+  },
+  methods: {
+    get() {
+      axios.get(`https://us-central1-comments-downloader.cloudfunctions.net/fetchComments/${this.videoId}`)
+      firebase.database().ref(`/video/${videoId}/`).on()
+    }
+  },
+  mounted() {
+    get()
+  }
+}
+</script>
+
+<style>
+
+</style>
